51f6f4f326d16ce3a1df26c216782373f9ced1ec
Forketyfork <forketyfork@icloud.com>
2025-05-28 14:06:25 +0200

    Add Jest code coverage

--- jest.config.js
+++ jest.config.js
@@ -7,5 +7,13 @@ module.exports = {
 	testEnvironment: "jsdom",
 	transform: {
 		...tsJestTransformCfg,
-	},
+       },
+	coverageProvider: "v8",
+	coverageDirectory: "coverage",
+	collectCoverageFrom: [
+		"**/*.ts",
+		"!**/*.test.ts",
+		"!**/__tests__/**",
+		"!node_modules/**",
+	],
 };
